a2ee6c05ed9091521b65b86d64cd4fa5bdd560a5
nekokusa420 <sk011010ab@gmail.com>
2024-02-04 19:22:38 +0800

    98

--- src/ProductList.js
+++ src/ProductList.js
@@ -1,24 +1,28 @@
-import React from 'react'
+import React from 'react';
 import logo from './React-icon.svg.png'
 import styles from './ProductList.module.css'
-import {useState} from "react" //ReactHook
+import {useState, useEffect} from "react" //ReactHook
 import {Link} from "react-router-dom"
 import Title from './Title'
 
 export default function ProductList() {
 
-    let productList = [
-        {"id" : 1, "name" : "蘋果", "price" : 5,"image" : "apple.jpeg","description" : "新鮮蘋果50克, 一日一蘋果, 醫生遠離我"},
-        {"id" : 2, "name" : "橙", "price" : 3,"image" : "orange.jpeg","description" : "新鮮橙50克, 又甜又好吃"},
-        {"id" : 3, "name" : "芒果", "price" : 4,"image" : "mango.jpg","description" : "新鮮芒果500克, 宜做甜品"},
-        {"id" : 4, "name" : "西瓜", "price" : 20,"image" : "watermelon.jpeg","description" : "新鮮西瓜2公斤, 夏季必備"},
-        {"id" : 5, "name" : "藍梅", "price" : 10,"image" : "blueberry.webp","description" : "新鮮藍梅50克, 補眼之寶"},
-        {"id" : 6, "name" : "白蘿蔔", "price" : 5, "image" : "white-carrot.png", "description" : "新鮮白蘿蔔1公斤, 宜煲湯"}
-    ]
+let[productList, setProductList] = useState([])
 
-fetch('https://hoyinleung.github.io/demoapi/react-basic-product.json')
-    .then(respone => respone.json())
-    .then(data => console.log(data))
+useEffect(()=>{
+  //1 : 無第二個參數 : component每次render都會觸法
+  //2 : Dependency Array是空array時 : 只會在第一次網頁render時觸發
+  //3 : Dependency Array是有變數時 : 第一次網頁render時 + 指定的變數改變 會觸發
+
+
+  fetch('https://raw.githubusercontent.com/nekokusa420/first-spa/main/src/react-basic-product.json?token=GHSAT0AAAAAACNYMENPEQZEEJ3TKDX4IV3CZN7LLGA')
+  .then(respone => respone.json())
+  .then(data => setProductList(data))
+
+  console.log(productList)
+},[])
+
+
 
     const[showProduct, setShowProduct] = useState(true)
 
